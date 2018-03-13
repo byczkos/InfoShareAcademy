33718963d2dc8b99937e45d960ef571f9d365573
Dawid Byczkowski <dbyczkowski@gmail.com>
2018-03-13 13:30:50 +0100

    Added comment to json parser

--- RestAPI/google-client/src/main/java/ws/client/Data.java
+++ RestAPI/google-client/src/main/java/ws/client/Data.java
@@ -6,6 +6,10 @@ import java.util.List;
 public class Data {
     List<Translation> translations;
 
+    // Aby parsowac jsona nalezy stworzyc klasy odpowiadajace obiektom z formatu json. Wazne jest by stworzyc obiekt nadrzedny
+    // ktorego nie ma w pliku json. Nawiasy [] oznaczaja rozpoczacie sie listy i pamietac nalezy, by w obiektach odwzorowac to
+    // rowniez w postaci listy w Javie.
+    
     public List<Translation> getTranslations() {
         return translations;
     }
